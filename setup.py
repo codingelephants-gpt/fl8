6ac8d9f62ac0da73c6bc3e75be41984785237ab5
codingelephants <florismooij2014@gmail.com>
2024-12-18 16:55:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='fl8',
+   version='1.0',
+   description='File Locator and Packager',
+   author='codingelephants-gpt',
+   packages=find_packages(),
+)
